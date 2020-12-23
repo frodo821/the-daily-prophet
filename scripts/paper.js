99eaaf208a85b68aa7bf5b2d376843615d042c64
frodo821 <sakaic2003@gmail.com>
2020-12-23 23:56:43 +0900

    first commit

---
+++ scripts/paper.js
@@ -0,0 +1,7 @@
+function writeDate() {
+  let date = new Date();
+  let year = date.getFullYear();
+  let month = (date.getMonth() + 1).toString().padStart(2, '0');
+  let day = date.getDate().toString().padStart(2, '0');
+  document.write(`${year}/${month}/${day}`);
+}
